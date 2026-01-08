a48dfa397efc21c9cdab0f5b5180959eca07aa8a
Soham Charjan <soham0yt@gmail.com>
2026-01-08 17:42:07 +0530

    add analysis using and enhanced the embeded search.

--- backend/app/core/embed.py
+++ backend/app/core/embed.py
@@ -14,33 +14,3 @@ def embed_text(text: list) -> list:
     clusters = util.community_detection(embeddings, min_community_size=2, threshold=THRESHOLD)
 
     return clusters
-
-"""
-# clean the embed_text output and return only necessary metrix
-def clean_embed(coll: list) -> list:
-    # evecol is the list of lists that contain 2 indexes of the similar sections
-    evecol = []
-
-    # this sorts the sections
-    for i in range(len(coll)):
-        alr = set()
-
-        if coll[i][1] not in alr or coll[i][2] not in alr:
-            if coll[i][1] not in alr:
-                alr.add(coll[i][1])
-            if coll[i][2] not in alr:
-                alr.add(coll[i][2])
-
-        if list(alr) not in evecol:
-            evecol.append(list(alr))
-
-    return evecol
-"""
-
-"""
-    for i in range(len(text)):
-        for j in range(i + 1, len(text)):
-            score = round(float(similarities_matrix[i][j]))
-            if score > THRESHOLD:
-                collections.append((i, j, score))
-"""
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,37 @@
 # this file is for the fastAPI entry point and not reserved for any logic
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from core.extract_text import extract_text_from_pdf
 from core.section import text_splitter
 from core.embed import embed_text
-from api.analyze import sort_document, analyze_document
+from api.analyze import analyze_document
+from pydantic import BaseModel
 import json
+
+
+class Found(BaseModel):
+    separated : list[list[str]]
+    index : list[int]
+    title : list[str]
+    confidence : list[float]
+    issue : list[str]
+
 
 #initialize the fastAPI as app
 app = FastAPI(title="DCC - Document Completeness Checker")
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # returns a simple "statues ok" message when reached out to /health
 @app.get("/health")
@@ -16,7 +39,7 @@ async def health_check():
     return {"status": "ok"}
 
 #able to upload PDF when reached out to /upload-pdf
-@app.post("/upload-pdf")
+@app.post("/upload-pdf", response_model=Found)
 async def upload_pdf(file: UploadFile = File(...)):
     # the function outputs either a str or an error code:-
     # str: perfect text
@@ -29,6 +52,7 @@ async def upload_pdf(file: UploadFile = File(...)):
     result = json.loads(extract_text_from_pdf(file).body.decode('utf-8'))
     separated = text_splitter(result["text"])
     embedded = embed_text(separated)
+    analyzed = analyze_document(embedded=embedded, text=separated)
 
     # see if the result has an error
     if "error" in result:
@@ -39,11 +63,10 @@ async def upload_pdf(file: UploadFile = File(...)):
         elif result["error"] == 3:
             raise HTTPException(status_code=400, detail="Syntax error in the PDF file.")
         else:
-            raise HTTPException(status_code=500, detail= result["error"])
+            raise HTTPException(status_code=400, detail= result["error"])
 
     # if the result does not contain an error, push the result
     else:
+        print(analyzed)
+        raise HTTPException(status_code=200)
 
-        print(analyze_document(embedded=embedded, text=separated))
-        print(enumerate(separated))
-        return HTTPException(status_code=200, detail= embedded)
